0c81a6a3eaed40feceee743b27982c9169b88ef5
Keshav Madhav <keshav2552003@gmail.com>
2024-06-01 17:23:22 +0530

    Stored user infor to firebase db

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Navigate, Route, Routes, useNavigate } from 'react-router-dom'
 import { Home } from './container'
-import { auth } from './config/firebase.config'
+import { auth, db } from './config/firebase.config'
+import { doc, setDoc } from 'firebase/firestore'
+import { Spinner } from './components'
 
 const App = () => {
   const navigate = useNavigate()
+  const [isloading, setIsLoading] = useState(true)
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
         console.log(user.providerData[0])
+        setDoc(doc(db, "users", user.uid), user.providerData[0]).then(() => {
+          //
+        }).catch((error) => {
+          console.error("Error adding document: ", error)
+        })
       } else {
         navigate("/home/auth", {replace: true})
       }
+      setInterval(()=>{
+        setIsLoading(false)
+      }, 2000)
     })
-  }, [])
-  return (
-    <div className='w-screen h-screen flex items-start justify-start overflow-hidden'>
-      <Routes>
-        <Route path='/home/*' element={<Home />} />
 
-        <Route path='*' element={<Navigate to={"/home"}/>} />
-      </Routes>
-    </div>
+    return () => unsubscribe();
+  }, [])
+
+  return (
+    <>
+      {isloading ? (
+        <div className='w-screen h-screen flex items-center justify-center overflow-hidden'>
+          <Spinner />
+        </div>
+      ):(
+        <div className='w-screen h-screen flex items-start justify-start overflow-hidden'>
+          <Routes>
+            <Route path='/home/*' element={<Home />} />
+    
+            <Route path='*' element={<Navigate to={"/home"}/>} />
+          </Routes>
+        </div>
+      )}
+    </>
   )
 }
 
