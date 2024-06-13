cb493b84d57d98768b9d85d290fbba4589d158c7
Keshav Madhav <keshav2552003@gmail.com>
2024-06-13 10:54:33 +0530

    Added representation of likes in projects and updated favicon

--- src/container/Projects.jsx
+++ src/container/Projects.jsx
@@ -43,9 +43,9 @@ const ProjectCard = ({ project, index }) => {
           likes: newLikes,
         }
         setDoc(doc(db, 'Projects', project.id), _doc).then((res)=> {
-          toast.success(`You have unliked ${project.title}`)
+          toast.error(`You have unliked "${project.title}"`)
         }).catch((err)=> {
-          toast.error('Error while unliking the project')
+          toast.warning('Error while unliking the project')
           console.log(err)
         })
       }
@@ -58,9 +58,9 @@ const ProjectCard = ({ project, index }) => {
           dislikes: newDislikes,
         }
         setDoc(doc(db, 'Projects', project.id), _doc).then((res)=> {
-          toast.success(`You have liked ${project.title}`)
+          toast.success(`You have liked "${project.title}"`)
         }).catch((err)=> {
-          toast.error('Error while liking the project')
+          toast.warning('Error while liking the project')
           console.log(err)
         })
       }
@@ -77,9 +77,9 @@ const ProjectCard = ({ project, index }) => {
           dislikes: newDislikes,
         }
         setDoc(doc(db, 'Projects', project.id), _doc).then((res)=> {
-          toast.success(`You have removed dislike from ${project.title}`)
+          toast.success(`You have removed dislike from "${project.title}"`)
         }).catch((err)=> {
-          toast.error('Error while removing dislike from the project',{
+          toast.warning('Error while removing dislike from the project',{
             description: "This action has removed your dislike from the project if you disliked it before."
           })
           console.log(err)
@@ -94,11 +94,11 @@ const ProjectCard = ({ project, index }) => {
           likes: newLikes,
         }
         setDoc(doc(db, 'Projects', project.id), _doc).then((res)=> {
-          toast.success(`You have disliked ${project.title}`,{
+          toast.error(`You have disliked "${project.title}"`,{
             description: "This action has removed your like from the project if you liked it before."
           })
         }).catch((err)=> {
-          toast.error('Error while disliking the project',{})
+          toast.warning('Error while disliking the project',{})
           console.log(err)
         })
       }
@@ -106,12 +106,9 @@ const ProjectCard = ({ project, index }) => {
   }
 
   return (
-    <motion.div
+    <div
       key={index} 
       className='h-[48%] sm:w-[360px] bg-secondary rounded-md p-3 flex flex-col items-center justify-center gap-3 cursor-pointer'
-      initial={{opacity: 0, y: 30}}
-      animate={{opacity: 1, y: 0}}
-      exit={{opacity: 0, y: 30}}
       onClick={() => navigate(`/openProject/${project.id}`)}
     >
       <div className='bg-primary w-full h-full rounded-md overflow-hidden' style={{overflow:"hidden", height:"100%"}}>
@@ -142,11 +139,36 @@ const ProjectCard = ({ project, index }) => {
         </div>
 
         <div className='flex items-center justify-start gap-3'>
-          <FaThumbsUp onClick={(e)=>handleUpVote(e)} className='text-white/20 text-xl hover:text-white/30'/>
-          <FaThumbsDown onClick={(e)=>handleDownVote(e)} className='text-white/20 text-xl hover:text-white/30'/>
+          <div className='flex items-center justify-start gap-1'>
+            <p className={` text-xs mt-0.5 ${currUser && project.likes?.includes(currUser.uid) ? "text-emerald-700/80" : " text-white/70"}`}>{project.likes?.length || 0}</p>
+            <motion.div
+              className='flex items-center justify-center gap-1'
+              whileHover={{scale: 1.2}}
+              title={`Likes: ${project.likes?.length || 0}`}
+            >
+              <FaThumbsUp 
+                onClick={(e)=>handleUpVote(e)} 
+                className={`text-xl mb-1 ${currUser && project.likes?.includes(currUser.uid) ? "text-emerald-700/80 hover:text-emerald-700" : "hover:text-white/30 text-white/20"}`}
+              />
+            </motion.div>
+          </div>
+
+          <div className='flex items-center justify-start gap-1'>
+            <p className={`text-xs mt-0.5 ${currUser && project.dislikes?.includes(currUser.uid) ? "text-red-700/80" : " text-white/70"} text-sm`}>{project.dislikes?.length || 0}</p>
+            <motion.div
+              className='flex items-center justify-center gap-1'
+              whileHover={{scale: 1.2}}
+              title={`Likes: ${project.dislikes?.length || 0}`}
+            >
+              <FaThumbsDown 
+                onClick={(e)=>handleDownVote(e)} 
+                className={`text-xl mt-0.5 ${currUser && project.dislikes?.includes(currUser.uid) ? "text-red-700/80 hover:text-red-700" : "hover:text-white/30 text-white/20"}`}
+              />
+            </motion.div>
+          </div>
         </div>
       </div>
-    </motion.div>
+    </div>
   )
 }
 
